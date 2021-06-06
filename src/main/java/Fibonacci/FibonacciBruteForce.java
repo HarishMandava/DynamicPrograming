4b669d0ab4a3371a2352026ba9da67f88acabfc0
harishmandava <harishmandavaaaa@gmail.com>
2021-06-05 20:08:07 -0500

    Adds Fibonacci program(brute force)

---
+++ src/main/java/Fibonacci/FibonacciBruteForce.java
@@ -0,0 +1,19 @@
+package Fibonacci;
+
+public class FibonacciBruteForce {
+
+    public static long fibonacci(int n) {
+        if(n <= 2) {
+            return 1;
+        }
+        return fibonacci(n-1) + fibonacci(n-2);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(FibonacciBruteForce.fibonacci(6));
+        System.out.println(FibonacciBruteForce.fibonacci(7));
+        System.out.println(FibonacciBruteForce.fibonacci(50));
+    }
+
+    // Time Complexity : 0(2 ^ N)  Space Complexity : 0(N)
+}
